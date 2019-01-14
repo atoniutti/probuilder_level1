2c72e1002666ca93f7d159199f9a481dfb0fc5b6
Toniutti Andrea <atoniutti002@ensc.fr>
2019-01-14 14:18:58 +0100

    meta files

---
+++ Formation JE/Assets/Scripts/CharacterInteraction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterInteraction : MonoBehaviour {
+
+    public GameObject tpGoal;
+    public Text flouzText;
+
+    public int flouz;
+
+	// Use this for initialization
+	void Start ()
+    {
+        flouz = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Portal")
+        {
+            gameObject.transform.position = tpGoal.transform.position;
+        }
+        else if (col.gameObject.tag == "gold")
+        {
+            flouz++;
+            Destroy(col.gameObject);
+            flouzText.text = flouz.ToString();
+        }
+    }
+
+}
